9c6d9a9d0a1037bf63ad467061d345540deb219e
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-09 21:24:15 +0600

    practise

---
+++ bin/operators/arithmetic.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 25;
+  int b = 10;
+  print(a + b); // Add
+  print(a - b); // Subtract
+  print(a * b); // Multiply
+  print(a / b); // Divide
+  print(a ~/ b); // Divide, returning an integer result
+  print(a % b); // Get the remainder of an integer division (modulo)
+}
---
+++ bin/operators/equality_and_relational.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("Equality and relational operators");
+  int a = 15;
+  int b = 16;
+  print(a == b); // Equal
+  print(a != b); // Not equal
+  print(a > b); // Greater than
+  print(a < b); // Less than
+  print(a >= b); // Greater than or equal to
+  print(a <= b); // Less than or equal to
+}
